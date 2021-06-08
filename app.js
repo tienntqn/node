e761fefec82fa79631ce3a6a42099553c4bb9601
Tien Nguyen <tienntqn@gmail.com>
2021-06-08 13:35:13 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/' , (req , res)=>{
+
+   res.send('<h1>Hello from simple server</h1>')
+
+});
+
+app.listen(3000,()=>{
+    console.log("Server starting...");
+});
